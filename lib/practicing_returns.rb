789f7f0f712d0045db5dc09d7424484b80b2f9ba
osagie obaretin <osagie.codes@gmail.com>
2020-06-07 20:32:55 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,14 +1,13 @@
-require 'pry'
+
 
 def hello(array)
   i = 0
   
   while i < array.length
-    collection << yield(array[i])
+     yield(array[i])
     i += 1
   end
-   collection
+   
   end
-binding.pry
-hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+
 
